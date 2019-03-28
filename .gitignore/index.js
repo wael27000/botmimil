483d0aa0160a8033e68cdab72bbcadd5d096daf7
wael27000 <43893268+wael27000@users.noreply.github.com>
2019-03-28 12:58:55 +0100

    Update and rename .gitignore to index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.login('NTU5MTI4MjgyMzkwMzMxNDA5.D3g4Xw.92gl4mKaLLTHEBWcNmFS86na9MM');
+Client.on("ready", () =>{
+    console.log("je suis prêt")
+Client.user.setGame("zidez Mimil")});
+
+
+
